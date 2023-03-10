5988e26a6ca4ad43a0e130f70edc51dbe8339d96
William <80749210+Ephraim201@users.noreply.github.com>
2023-03-10 17:19:14 +0100

    Add files via upload

---
+++ Task1/Task1/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label19 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(1033, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(42, 17);
+            this.label19.TabIndex = 11;
+            this.label19.Text = "None";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(888, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(76, 17);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "VB version";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(794, 130);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(42, 17);
+            this.label18.TabIndex = 10;
+            this.label18.Text = "None";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(662, 130);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(90, 17);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "VB Installed?";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(526, 130);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(42, 17);
+            this.label20.TabIndex = 12;
+            this.label20.Text = "None";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(324, 130);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(131, 17);
+            this.label14.TabIndex = 15;
+            this.label14.Text = "Internet Connection";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(108, 130);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(42, 17);
+            this.label17.TabIndex = 9;
+            this.label17.Text = "None";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(3, 130);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(85, 17);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "MAC Adress";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(324, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 17);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Hostname";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(662, 65);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 17);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "SSID Status";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "None";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(526, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 17);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "None";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(662, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SSID";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(794, 65);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 17);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "None";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(108, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "None";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(794, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 17);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "None";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "None";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "IP Gateway";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Host";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.66476F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.33524F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 202F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 136F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 145F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 153F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label11, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label7, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label13, 5, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label9, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label10, 4, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label12, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label17, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label14, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label20, 3, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label15, 4, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label18, 5, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label16, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label19, 7, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(37, 197);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1184, 210);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 516);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ Task1/Task1/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using SimpleWifi;
+using System.Net;
+using Microsoft.Win32;
+using System.Diagnostics;
+using Microsoft.WindowsAPICodePack.Net;
+using System.IO;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+
+        public void CollectIP()
+        {
+            string ipAddress = "";
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            ipAddress = ip.Address.ToString();
+                            label2.Text = ipAddress;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GateWay()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties ipProps = ni.GetIPProperties();
+                    foreach (GatewayIPAddressInformation gw in ipProps.GatewayAddresses)
+                    {
+                        Console.WriteLine("Gateway IP Address: " + gw.Address.ToString());
+                        label7.Text = gw.Address.ToString();
+                    }
+                }
+            }
+        }
+
+        public void MacAdress()
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(n => n.OperationalStatus == OperationalStatus.Up);
+
+            if (networkInterface != null)
+            {
+                var macAddress = networkInterface.GetPhysicalAddress().ToString();
+
+                Console.WriteLine($"La dirección MAC de la interfaz de red es: {macAddress}");
+                label17.Text = macAddress;
+            }
+            else
+            {
+                Console.WriteLine("No se encontró una interfaz de red activa.");
+                label17.Text = "No hay";
+            }
+        }
+
+        public void Username()
+        {
+            var userName = Environment.UserName;
+
+            Console.WriteLine($"El nombre de usuario actual es: {userName}");
+            label4.Text = userName;
+        }
+
+        public void Hostname()
+        {
+            var hostName = Dns.GetHostName();
+
+            label9.Text = hostName;
+        }
+
+        public void InternetCollection()
+        {
+            var ping = new Ping();
+            var result = ping.Send("www.google.com");
+
+            if (result.Status == IPStatus.Success)
+            {
+                Console.WriteLine("El equipo está conectado a Internet.");
+                label20.Text = "Conectado";
+                label20.ForeColor = Color.Green;
+            }
+            else
+            {
+                Console.WriteLine("El equipo no está conectado a Internet.");
+                label20.Text = "Desconectado";
+            }
+        }
+
+        // Obtener SSID y su status
+        public void SSID()
+        {
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up && networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    label11.Text = networkInterface.Name;
+                    label13.Text = ($"{networkInterface.OperationalStatus}");
+                    label13.ForeColor = Color.Green;
+                    
+                }
+            }
+
+        }
+
+        public void ProgramInstalled()
+        {
+            if (File.Exists("C:/Program Files/Oracle/VirtualBox/VBoxManage.exe"))
+            {
+                string[] files = Directory.GetFiles(@"C:\Program Files\Oracle\VirtualBox", "VBoxManage.exe", SearchOption.AllDirectories);
+
+                if (files.Length > 0)
+                {
+                    label18.Text= "Instalado";
+                }
+                else
+                {
+                    label18.Text = "No esta instalado";
+                }
+            }
+            else
+            {
+                label18.Text = "No esta instalado";
+            }
+        }
+
+        public void ProgramVersion()
+        {
+            if (File.Exists("C:/Program Files/Oracle/VirtualBox/VBoxManage.exe"))
+            {
+                var version = FileVersionInfo.GetVersionInfo("C:/Program Files/Oracle/VirtualBox/VBoxManage.exe").FileVersion;
+
+                label19.Text = version;
+            }
+            else
+            {
+                label19.Text = "---";
+            }
+        }
+
+
+        public void MakeUp()
+        {
+            //No-resize
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //icono
+            //this.Icon = new Icon("D:/TrabajosProgramacion/CShardVisualStudio/Task1/Task1/icon.ico");
+
+            //Nombre
+            this.Text = "Proyeto 1 (Nombre provicional)";
+            this.BackColor = Color.Black;
+
+            //----------------- Fonts ---------------------- 
+
+            //Grueso
+            label1.Font = new Font(label1.Font, FontStyle.Bold);
+            label6.Font = new Font(label6.Font, FontStyle.Bold);
+            label12.Font = new Font(label12.Font, FontStyle.Bold);
+
+            label3.Font = new Font(label3.Font, FontStyle.Bold);
+            label8.Font = new Font(label8.Font, FontStyle.Bold);
+            label14.Font = new Font(label14.Font, FontStyle.Bold);
+
+            label5.Font = new Font(label5.Font, FontStyle.Bold);
+            label10.Font = new Font(label10.Font, FontStyle.Bold);
+            label15.Font = new Font(label15.Font, FontStyle.Bold);
+            label16.Font = new Font(label16.Font, FontStyle.Bold);
+
+            //---------------- Color -----------------------
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            label3.ForeColor = Color.White;
+            label4.ForeColor = Color.White;
+            label5.ForeColor = Color.White;
+            label6.ForeColor = Color.White;
+            label7.ForeColor = Color.White;
+            label8.ForeColor = Color.White;
+            label9.ForeColor = Color.White;
+            label10.ForeColor = Color.White;
+            label11.ForeColor = Color.White;
+            label12.ForeColor = Color.White;
+            label13.ForeColor = Color.White;
+            label14.ForeColor = Color.White;
+            label15.ForeColor = Color.White;
+            label16.ForeColor = Color.White;
+            label17.ForeColor = Color.White;
+            label18.ForeColor = Color.White;
+            label19.ForeColor = Color.White;
+
+
+            //---------- Font respuesta -----------
+            label2.Font = new Font("Sans", 9);
+            label7.Font = new Font("Sans", 9);
+            label17.Font = new Font("Sans", 9);
+
+            label4.Font = new Font("Sans", 9);
+            label9.Font = new Font("Sans", 9);
+            label20.Font = new Font("Sans", 9);
+
+            label11.Font = new Font("Sans", 9);
+            label13.Font = new Font("Sans", 9);
+            label18.Font = new Font("Sans", 9);
+
+            label19.Font = new Font("Sans", 9);
+        }
+
+        
+
+
+        // main
+        public Form1()
+        {
+            //Todo
+            InitializeComponent();
+            MakeUp();
+
+            //Acciones
+            CollectIP();
+            GateWay();
+            MacAdress();
+            Username();
+            Hostname();
+            InternetCollection();
+            ProgramInstalled();
+            ProgramVersion();
+            SSID();
+        }
+
+
+
+        //-------------------- LABELS?--------------------------
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
